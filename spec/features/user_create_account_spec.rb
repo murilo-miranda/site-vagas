e06f4723f861791c73bbbc8cf9f5f5f5a59f6421
m-faulkness <murilomirandamoreno@gmail.com.com>
2021-02-13 15:05:16 -0300

    + shoulda_matchers gem

--- spec/features/user_create_account_spec.rb
+++ spec/features/user_create_account_spec.rb
@@ -2,31 +2,29 @@ require 'rails_helper'
 
 feature 'User create account' do
   scenario 'successfully' do
-    collaborator = User.new(email: 'murilo@campuscode',
+    user = User.new(email: 'murilo@campuscode',
                                 password: '123456')
 
     visit root_path
     click_on 'Registrar-se'
-    fill_in 'user[email]', with: collaborator.email
-    fill_in 'user[password]', with: collaborator.password
-    fill_in 'user[password_confirmation]', with: collaborator.password
-    click_on 'Sign up'
+    fill_in 'user[email]', with: user.email
+    fill_in 'user[password]', with: user.password
+    fill_in 'user[password_confirmation]', with: user.password
+    click_on 'Cadastre-se'
 
     expect(page).to have_content 'Bem vindo! Você realizou seu registro com sucesso.'
-    expect(page).to have_content collaborator.email
+    expect(page).to have_content user.email
     expect(page).to have_link 'Sair'
   end
 
   scenario 'unsuccessfully - blank field' do
 
     visit root_path
-    click_on 'Entrar'
     click_on 'Registrar-se'
-    click_on 'Sign up'
+    click_on 'Cadastre-se'
 
     expect(page).to have_content 'Não foi possível salvar usuário: 2 erros' #I18n
     expect(page).to have_content 'E-mail não pode ficar em branco'
     expect(page).to have_content 'Senha não pode ficar em branco'
-    click_on 'Sign up'
   end
 end
--- spec/features/user_sign_spec.rb
+++ spec/features/user_sign_spec.rb
@@ -2,28 +2,27 @@ require 'rails_helper'
 
 feature 'User sign' do
   scenario 'successfully' do
-    collaborator = User.create!(email: 'murilo@campuscode',
+    user = User.create!(email: 'murilo@campuscode',
                                 password: '123456')
 
     visit root_path
-    click_on 'Entrar' #Sign in
-    fill_in 'user[email]', with: collaborator.email
-    fill_in 'user[password]', with: collaborator.password
-    click_on 'Log in'
+    click_on 'Entrar'
+    fill_in 'user[email]', with: user.email
+    fill_in 'user[password]', with: user.password
+    click_on 'Entrar'
 
-    expect(page).to have_content 'Login efetuado com sucesso!' #i18n
-    expect(page).to have_content collaborator.email
+    expect(page).to have_content 'Login efetuado com sucesso!'
+    expect(page).to have_content user.email
     expect(page).to have_link 'Sair'
-    expect(page).not_to have_link 'Entrar' #i18n
+    expect(page).not_to have_link 'Entrar'
   end
 
   scenario 'unsuccessfully - blank field' do
 
     visit root_path
     click_on 'Entrar'
-    click_on 'Log in'
+    click_on 'Entrar'
 
-    expect(page).to have_content 'E-mail ou senha inválida.' #I18n
-    expect(page).to have_link 'Entrar' #I18n
+    expect(page).to have_content 'E-mail ou senha inválida.'
   end
 end
